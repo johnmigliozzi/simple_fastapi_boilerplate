b26ff526f64837afc8c7bd0714f48798b9f5af72
johnmigliozzi <john.migliozzi@gmail.com>
2025-06-04 21:16:05 -0400

    "real" security (from FastAPI tutorial)

--- main.py
+++ main.py
@@ -1,49 +1,60 @@
 from fastapi import FastAPI, Request, Depends, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import jwt
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+
 from typing import Annotated
 from pydantic import BaseModel
-import datetime
+from datetime import datetime, timedelta, timezone
 import sqlite3
 import pandas as pd
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "85935d4cfcdbce5b38b823fa0f453be55a27e0fa863098d8f126f2aa88be874a"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 3
 
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
+        "hashed_password": '$2b$12$RYQixhK.3ntjpSSeezp39O6I2drAhZXRsTFchrAtIL04uE3KNeXTS',
         "disabled": False,
     },
     "alice": {
         "username": "alice",
         "full_name": "Alice Wonderson",
         "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
+        "hashed_password": '$2b$12$XlwlFZbgd9Qo3UAl.DARK.4r8/7KbgA0TY53dFu5xV6FDDD5EojYC',
         "disabled": True,
     },
     "danielle": {
         "username": "danielle",
         "full_name": "danielle",
         "email": "danielle@example.com",
-        "hashed_password": "fakehashedsecret3",
+        "hashed_password": '$2b$12$OwatVfC6sT/ymYXfCsEOBeZoygeUwh.XQUZS4..k1bZwAGj1d32hK',
         "disabled": False,
     },
     "john": {
         "username": "john",
         "full_name": "john",
         "email": "john@example.com",
-        "hashed_password": "fakehashedsecret4",
+        "hashed_password": '$2b$12$5lJ45GFbybzjvBz0UVc7PecSVFbjx2tg2y57Tf73blU3e/t5olccC',
         "disabled": False,
     },
 }
 
-app = FastAPI()
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+class TokenData(BaseModel):
+    username: str | None = None
 
 class User(BaseModel):
     username: str
@@ -54,26 +65,58 @@ class User(BaseModel):
 class UserInDB(User):
     hashed_password: str
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
 
-
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
     return user
 
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
 
@@ -86,23 +129,34 @@ async def get_current_active_user(
 
 
 @app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
-    return {"access_token": user.username, "token_type": "bearer"}
 
-
-@app.get("/users/me")
+@app.get("/users/me", response_model=User)
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)],
 ):
     return current_user
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
 
 @app.get("/items/")
 async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
@@ -111,6 +165,7 @@ async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
 
 @app.get("/get_task/{id}")
 def get_task(id: int, current_user: Annotated[User, Depends(get_current_active_user)]):
+# def get_task(id: int):
     con = sqlite3.connect("./data.db")
     sel_task = pd.read_sql_query(
         f"SELECT * FROM completed_tasks WHERE id = {id}", 
@@ -120,11 +175,11 @@ def get_task(id: int, current_user: Annotated[User, Depends(get_current_active_u
     return sel_task.iloc[0].to_dict()
 
 @app.put("/put_task/")
-def put_task(date: datetime.datetime, task_name: str, project_name: str):
+def put_task(date: datetime, task_name: str, project_name: str):
     con = sqlite3.connect("./data.db")
     cursor = con.cursor()
 
-    tmstp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    tmstp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     cursor.execute(f"INSERT INTO completed_tasks \
         (created_timestamp,override_date,task_name,project_name) \
@@ -136,7 +191,7 @@ def put_task(date: datetime.datetime, task_name: str, project_name: str):
 @app.get("/api/do_stuff/")
 def do_stuff():
     with open("./out.txt", "w") as file:
-        tmstp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        tmstp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"printed at: {tmstp}")
 
 app.mount("/", StaticFiles(directory="static",html = True), name="static")
--- static/app.js
+++ static/app.js
@@ -1,7 +1,34 @@
+// Authenticate
+async function token(username,password) {
+    fetch('/token', {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `&username=${username}&password=${password}`
+    }).then(res => {
+        return res.json();
+    }).then(json => {
+        console.log(json['access_token']);
+        fetch('/users/me', {
+            method: 'GET',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Bearer ' + json['access_token']
+            }
+        }).then(res => {
+            return res.json();
+        }).then(json => {
+            console.log(json);
+        });
+    });
+}
+
 // Accept any URL and return a JS object from a JSON API
-async function getJsonApiResponse(url) {
+async function getJsonApiResponse(url, myHeaders) {
     try {
-        const response = await fetch(url);
+        const response = await fetch(url, {headers: myHeaders});
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
@@ -22,7 +49,12 @@ async function getData() {
     var selectedDataId = document.getElementById("selected_data_id").value;
     const url = "http://127.0.0.1:8000/get_task/" + selectedDataId;
 
-    getJsonApiResponse(url)
+
+    const myHeaders = new Headers();
+    // myHeaders.append("username", "johndoe");
+    // myHeaders.append("password", "johndoesecret");
+
+    getJsonApiResponse(url, myHeaders)
         .then(data => {
             if (data) {
                 var contentStringData = "ID:  " + data['id'] + "<br/>";
